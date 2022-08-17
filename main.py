810795ea30742b820351b2177bde51119f091049
Daniel Woodson <s.dwoodson@gmail.com>
2022-08-17 11:39:40 -0700

    v0.2 - error checking and layout updates

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def get_dir():
 def create_pdf():
     start = int(start_entry.get())
     stop = int(stop_entry.get())
+    if start > stop:
+       start, stop = stop, start
     root.update_idletasks()
     global path
-    print(start, stop)
-
     pdf = FPDF('L', 'in', 'Letter')
     pdf.set_font('Arial', 'B', size=150)
     pdf.set_margins(0, 0.5)
@@ -37,43 +37,44 @@ def create_pdf():
         pdf.cell(10, 7.125 / 2, string, ln=1, align='C')
         pdf.cell(10, 7.125 / 2, string, ln=1, align='C')
     file_name = path + '/BM_Pallets ' + str(start) + '-' + str(stop) + '.pdf'
-    print(file_name)
     pdf.output(file_name, 'F')
 
 
 if __name__ == '__main__':
     n = 0
     root = tk.Tk()
-    root.geometry('325x200')
+    # root.geometry('325x200')
     root.title('Bay Materials Pallet Labels')
-    tk.Label(root, text="1. Enter a start and stop \"BM\" number \n"
+    frame = tk.Frame(root, relief='sunken')
+    frame.pack(fill='both', expand=True, padx=10, pady=10)
+    tk.Label(frame, text="1. Enter a start and stop \"BM\" number \n"
                         "2. Select a file location to store the file\n"
-                        "3. Open the pdf, verify it's correct and print\n", justify='left').grid(row=n, columnspan=2)
+                        "3. Open the pdf, verify it's correct and print\n", justify='left').grid(row=n, columnspan=2, sticky='w')
     n += 1
 
-    tk.Label(root, text="Start Number", justify='left').grid(row=n)
-    start_entry = tk.Entry(root)
+    tk.Label(frame, text="Start Number").grid(row=n, sticky='w')
+    start_entry = tk.Entry(frame)
     start_entry.insert(0, "0")
-    start_entry.grid(row=n, column=1)
+    start_entry.grid(row=n, column=1, sticky='e')
     n += 1
 
-    tk.Label(root, text="Stop Number", justify='left').grid(row=n)
-    stop_entry = tk.Entry(root)
+    tk.Label(frame, text="Stop Number", justify='left').grid(row=n, sticky='w')
+    stop_entry = tk.Entry(frame)
     stop_entry.insert(0, "0")
-    stop_entry.grid(row=n, column=1)
+    stop_entry.grid(row=n, column=1, sticky='e')
     n += 1
 
-    tk.Label(root, text="File location:", justify='left').grid(row=n)
+    tk.Label(frame, text="File location:", justify='left').grid(row=n, sticky='w')
     n += 1
 
-    file_location_entry = tk.Entry(root, justify='left')
-    file_location_entry.grid(row=n)
+    file_location_entry = tk.Entry(frame, justify='left', width=38)
+    file_location_entry.grid(row=n, columnspan=2, sticky='w')
     n += 1
 
-    tk.Button(root, text="Select Folder", command=get_dir).grid(row=n)
+    tk.Button(frame, text="Select Folder", command=get_dir).grid(row=n, sticky='w')
     n += 1
 
     root.update_idletasks()
-    tk.Button(root, text="Create PDF", command=create_pdf).grid(row=n)
+    tk.Button(frame, text="Create PDF", command=create_pdf).grid(row=n, sticky='w')
 
     root.mainloop()
